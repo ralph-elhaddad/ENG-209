65ff6d54c77929a7e7aa712b3054912f8ef3c6a5
Ralphcpp <48493572+Ralphcpp@users.noreply.github.com>
2019-10-13 21:03:39 +0200

    Create Camion.cpp

---
+++ Camion.cpp
@@ -0,0 +1,16 @@
+ * Camion.cpp
+ *
+ *  Created on: Oct 12, 2019
+ *      Author: pcoo21
+ */
+
+#include "Camion.h"
+#include <iostream>
+
+namespace Parking {
+
+double Camion::get_charge_utile(){
+	return charge_utile;
+}
+
+} /* namespace Parking */
